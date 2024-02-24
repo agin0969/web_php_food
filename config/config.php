c5c88f9e0acabbb51ff335420da42f5359a43a68
le trong thien <letrogthien@gmail.com>
2024-02-24 23:37:57 +0700

    gin 24/2

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?
+define('DB_Host', 'localhost');
+define('DB_Name', 'db_ct07');
+define('DB_User', 'root');
+define('DB_PassWord', 'mysql');
+?>
--- database/database.php
+++ database/database.php
@@ -1,8 +1,29 @@
 <?php
+include_once('../config/config.php');
 Class Database{
+    private $host= DB_Host;
+    private $name= DB_Name;
+    private $user=DB_User;
+    private $pass= DB_PassWord;
+    private $conn;
 
 
+    function __construct() {
+        $this->connect();
+    }
+    private function connect() {
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->name", $this->user, $this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+    private function query($sql) {
+        return $this->conn->query($sql);
+    }
 
-
-    
+    private function closeConn(){
+        $this->conn = null;
+    }
 }
