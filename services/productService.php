3660f382f71057cbb27d6494b5c5b87bc0149a3b
le trong thien <letrogthien@gmail.com>
2024-02-26 16:33:48 +0700

    chinh sua userservice va them moi product service

--- services/productService.php
+++ services/productService.php
@@ -6,5 +6,23 @@ class ProductService{
         $this->conn=new Database();
     }
 
+    public function getAllProduct(){
+        try {
+            $sql = "SELECT * FROM `product`";
+            $result = $this->conn->prepare($sql);
+            $result->execute();
+
+            if ($result) {
+                $products = $result->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                $products = array();
+            }
+        } catch (PDOException $e) {
+            die("Error: " . $e->getMessage());
+        } finally {
+            $this->conn->closeConn();
+        }
+        return $products;
+    }
 
 }
