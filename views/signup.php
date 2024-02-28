92a2d8ac3645229e9dce9f3a1a9b9762f7667117
Longne <thanhlongzzz47@gmail.com>
2024-02-28 09:28:24 +0700

    Them login.js

--- views/signup.php
+++ views/signup.php
@@ -12,6 +12,7 @@
         <img src="../resource/static/img/wefood.png" alt="Wefood logo">
         <h2>Tạo tài khoản của bạn</h2>
         <form action="../controllers/registerController.php" method="post">
+            
             <div class="input-field">
                 <input type="text" name="username" placeholder="Tài khoản" required>
             </div>
@@ -26,38 +27,6 @@
         <p class="signin-link">Đã có tài khoản? <a href="login.php">Đăng nhập</a></p>
     </div>
 </div>
-
+<script src="login.js"></script>
 </body>
 </html>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Lấy dữ liệu từ form
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    
-    if (!preg_match("/^[a-zA-Z0-9]+$/", $username)) {
-        echo "Tên tài khoản chỉ được chứa các ký tự chữ và số.";
-        exit;
-    }
-
-    
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Email không hợp lệ.";
-        exit; 
-    }
-
-    
-    if (strlen($password) < 8) {
-        echo "Mật khẩu phải có ít nhất 8 ký tự.";
-        exit; 
-    }
-
-  
-    echo "Đăng ký thành công!";
-    
-    header("Location: login.php");
-    exit; 
-}
-?>
