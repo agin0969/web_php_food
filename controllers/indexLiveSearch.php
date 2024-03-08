2f9c3bd4148f7f60d58a7763070a2953422989dc
le trong thien <letrogthien@gmail.com>
2024-03-09 01:46:18 +0700

    gin - realtime search

--- controllers/indexLiveSearch.php
+++ controllers/indexLiveSearch.php
@@ -9,13 +9,20 @@ $products=$productService->getProductByListId($ids);
 $tmp=count($products);
 if($tmp>5){
     $tmp=5;
-}
+}if($tmp===0){
+    echo'<button type="button" class="list-group-item list-group-item-action">nothing</button>';
 
+}
+ else {
 echo '<div class="list-group">';
-for ($i=0; $i < $tmp; $i++) {  echo'
-<button type="button" class="list-group-item list-group-item-action">'.$products[$i]->getName().'</button>';
+for ($i=0; $i < $tmp; $i++) {  
+    
+    
+    
+    echo'<button type="button" class="list-group-item list-group-item-action">'.$products[$i]->getName().'</button>';
 
 }
 echo '</div>';
 
 
+ }
