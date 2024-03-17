fc1e9edda3039a29a6adddd4d4d36647aa816755
le trong thien <letrogthien@gmail.com>
2024-03-17 16:36:37 +0700

    back to befor url- gin

--- controllers/proAddAdCon.php
+++ controllers/proAddAdCon.php
@@ -15,6 +15,9 @@ if (!$checkSession->checkSessionAdmin()){
     $productService = new ProductService();
     
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if($_POST['price']!="" && !is_numeric($_POST['price'])){
+            echo "<script>alert('Vui lòng nhập đúng kiểu dữ liệu cho các trường giá');  window.history.back();</script>";
+        } else {
         
             $name = $_POST['name'];
         $category_id =$_POST['category'];
@@ -23,13 +26,11 @@ if (!$checkSession->checkSessionAdmin()){
         
         $result=$productService->addProductToDb($name, $category_id, $price, $img, $description);   
         if($result){
-            header("Location: ../views/adminView.php");
+            echo "<script>window.history.back();</script>";
             exit();
         }
-        else {
-            header("Location: ../views/404.php");
-            exit();
-        }
+    }
+        
         
     }else {
         header("Location: ../views/404.php");
--- controllers/proChgAdCon.php
+++ controllers/proChgAdCon.php
@@ -14,18 +14,21 @@ $productService = new ProductService();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
-    $name = $_POST['name'];
-    $id = $_POST['id1'];
-    $category_id =$_POST['category'];
-    $price=$_POST['price'];
-    $description=$_POST['descrip'];
-    if($productService->changeProduct($id,$name, $category_id, $price, $img, $description)){
-        header("Location: ../views/adminView.php");
-        exit();
-    }  else {
-        header("Location: ../views/404.php");
-        exit;
+    if($_POST['price']!="" && !is_numeric($_POST['price'])){
+        echo "<script>alert('Vui lòng nhập đúng kiểu dữ liệu cho các trường giá');  window.history.back();</script>";
+    } else {
+        $name = $_POST['name'];
+        $id = $_POST['id1'];
+        $category_id =$_POST['category'];
+        $price=$_POST['price'];
+        $description=$_POST['descrip'];
+        if($productService->changeProduct($id,$name, $category_id, $price, $img, $description)){
+            echo "<script>window.history.back();</script>";
+            exit();
+        } 
+
     }
+    
 
 }  else {
     header("Location: ../views/404.php");
